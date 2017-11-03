7e9c41cc0ce58727ca4472f4ed391c5e9357edd1
andre~ <katona.andrei@gmail.com>
2017-11-03 17:29:18 +0100

    added product create

---
+++ ShopBusiness/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShopData;
+
+namespace ShopBusiness
+{
+    public class ProductController
+    {
+        ShopModel model;
+        Product product;
+        List<Product> products;
+        public ProductController()
+        {
+            model = new ShopModel();
+        }
+
+        public bool Create(string name, string description, int category, int stock, decimal price)
+        {
+            bool isCreated = true;
+            product = model.Products.SingleOrDefault(x => x.Name == name);
+            if (product == null)
+            {
+                product = new Product
+                {
+                    Name = name,
+                    Description = description,
+                    Category_Id = category,
+                    Stock = stock,
+                    Price = price
+                };
+                model.Products.Add(product);
+                model.SaveChanges();
+            }
+            else
+                isCreated = false;
+            return isCreated;
+        }
+    }
+}
